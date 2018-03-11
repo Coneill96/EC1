29743dce11757111c12434abbed8854b354ba601
Coneill96 <34420308+Coneill96@users.noreply.github.com>
2018-03-11 18:46:27 +0000

    Add files via upload

--- FProj/index.php
+++ FProj/index.php
@@ -1,1 +1,42 @@
-<?php echo "HELLO WORLD"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>EC1 Form login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <meta name="robots" context="follow">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.40/js/uikit-icons.min.js"></script>
+  
+ </head>
+
+ <body>
+ 	<div class="uk-section uk-container">
+ 		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-" uk-grid>
+ 			<form class="uk-form-stacked js-login">
+
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="form-stacked-text">Username</label>
+	        <div class="uk-form-controls">
+	            <input class="uk-input" id="form-stacked-text"  required="required"type="text" placeholder="B00......">
+	        </div>
+	    </div>
+
+	    <div class="uk-margin">
+	        <label class="uk-form-label" for="form-stacked-text">Password</label>
+	        <div class="uk-form-controls">
+	            <input class="uk-input" id="form-stacked-text" required="required" type="password" placeholder="Password">
+	        </div>
+	    </div>
+
+	    <div class="uk-margin">
+	    	<button class="uk-button uk-button-default" type="submit">Login</button>
+	   	</div>
+
+</form>
+ 	</div>
+ </body>
