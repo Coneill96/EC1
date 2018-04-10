9a66ed78da05b3eb11ed872be8b7d00f809858bb
Coneill96 <34420308+Coneill96@users.noreply.github.com>
2018-04-10 22:46:53 +0100

    Add files via upload

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2018 at 10:29 PM
+-- Generation Time: Apr 10, 2018 at 11:44 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -33,8 +33,38 @@ CREATE TABLE `assessment` (
   `ass_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `due_date` date NOT NULL COMMENT 'date assignment was due',
   `preferred_outcome` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `p_e_d` date NOT NULL COMMENT 'preferred extension date',
-  `request_id` int(5) NOT NULL
+  `p_e_d` date NOT NULL COMMENT 'preferred extension date'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `assessment`
+--
+
+INSERT INTO `assessment` (`ass_id`, `ass_name`, `due_date`, `preferred_outcome`, `p_e_d`) VALUES
+(2, 'Maths test', '2018-03-15', 'Another assessment oppurtunity', '2018-03-15'),
+(3, 'programming', '2018-03-16', 'Extension of Deadline', '2018-03-18'),
+(4, 'computer technologies', '2018-03-22', 'Deferred placement', '2018-03-24'),
+(5, 'Programming III', '2018-03-20', 'Extension of Deadline', '2018-03-29'),
+(9, 'programming III', '2018-04-11', 'Extension of Deadline', '2018-04-21'),
+(12, 'Maths', '2018-04-11', 'Extension of Deadline', '2018-04-14'),
+(13, 'Software Development', '2018-04-19', 'Extension of Deadline', '2018-04-20'),
+(14, 'Software systems', '2018-04-11', 'Deferred placement', '2018-04-19'),
+(15, 'Computing ', '2018-04-12', 'Extension of Deadline', '2018-04-21'),
+(16, 'Software systems', '2018-04-12', 'Extension of Deadline', '2018-04-20'),
+(17, 'Healthcare', '2018-04-12', 'Extension of Deadline', '2018-04-21'),
+(18, 'Maths', '2018-04-18', 'Extension of Deadline', '2018-04-14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pm_user`
+--
+
+CREATE TABLE `pm_user` (
+  `pm_id` int(5) NOT NULL,
+  `pm_username` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `course_name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pm_password` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -48,8 +78,26 @@ CREATE TABLE `reason_for_request` (
   `d_o_c` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'details of claim',
   `d_a_f` date NOT NULL COMMENT 'dates affected from',
   `d_a_t` date NOT NULL COMMENT 'dates affected to',
-  `evidence` blob NOT NULL COMMENT 'evidence'
+  `evidence` mediumblob NOT NULL COMMENT 'evidence',
+  `Statement` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `reason_for_request`
+--
+
+INSERT INTO `reason_for_request` (`rfr_id`, `d_o_c`, `d_a_f`, `d_a_t`, `evidence`, `Statement`) VALUES
+(4, 'I hurt my self', '2018-03-08', '2018-03-14', 0x433a5c66616b65706174685c5363616e6e656465762e706466, 'It was sore'),
+(5, 'I was sick', '2018-04-13', '2018-04-28', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'I would appreciate the extension'),
+(6, 'I was sick', '2018-04-13', '2018-04-28', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'I would appreciate the extension'),
+(7, 'I was sick', '2018-04-13', '2018-04-28', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'I would appreciate the extension'),
+(9, 'I was unwell and unable to attend', '2018-04-27', '2018-05-10', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'I hope to be back next week'),
+(10, 'I broke my wrist', '2018-04-20', '2018-04-19', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'I broke my wrist'),
+(11, 'Car broke down', '2018-04-20', '2018-04-20', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'The engine blew'),
+(12, 'Slept in', '2018-04-12', '2018-04-19', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'I slept in'),
+(13, 'There was a car crash', '2018-04-14', '2018-04-26', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'It disrupted the road to University'),
+(14, 'Broken wrist', '2018-04-20', '2018-04-27', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'iuhvifnv'),
+(15, 'Funeral', '2018-04-12', '2018-04-19', 0x433a5c66616b65706174685c546f616c73426f6f6b6d616b6572732e706466, 'I could not attend');
 
 -- --------------------------------------------------------
 
@@ -62,7 +110,9 @@ CREATE TABLE `requests` (
   `create_date` date NOT NULL COMMENT 'creation date of request',
   `sub_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'submission date',
   `status` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` int(5) NOT NULL
+  `user_id` int(5) UNSIGNED NOT NULL,
+  `ass_id` int(5) NOT NULL,
+  `rfr_id` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -75,25 +125,27 @@ CREATE TABLE `users` (
   `user_id` int(5) UNSIGNED NOT NULL COMMENT 'User Id',
   `Username` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User username',
   `password` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User password',
-  `email` int(250) NOT NULL COMMENT 'User email',
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User email',
   `reg_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time and date of registration',
   `first_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+  `surname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Users Table';
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `Username`, `password`, `email`, `reg_time`, `first_name`, `last_name`) VALUES
-(1, 'B00659788', '$2y$10$x0Xf8Vzm5zbHPq2my1c8TufQUf/GXcT1Bb7PFcMeAbBL5FxcML0x.', 0, '2018-03-14 11:52:10', '', ''),
-(2, 'B00659789', '$2y$10$UjLKUVR9B2vdCL2pQtpbiufXPCHya8lMss11gpHZPSIbg1qAdrcOG', 0, '2018-03-14 11:55:28', '', ''),
-(3, 'B00659785', '$2y$10$6TPeSODK91fFKz.1cpFGJeYu2r7v/WMYL88oI3HgaYEeE7lgCsJXu', 0, '2018-03-14 12:01:33', '', ''),
-(4, 'B00659786', '$2y$10$Qs60LSjYwfXUbiUoQG7xau9.hCUpo/P0P/lXFBKZhcAWfd957dY3S', 0, '2018-03-16 11:14:08', '', ''),
-(5, 'B00659781', '$2y$10$gbZaXv2EFdkconMqVRJ3y.CrV9Bj.l8of2PDz9ywKg1vWQK7OjamK', 0, '2018-03-21 18:47:07', '', ''),
-(6, 'B00654356', '$2y$10$keZXfor3kNmwJzMY2QLKnebU6jgZsUgAmtOH68xUjCL3wthvTlJ82', 0, '2018-03-22 16:44:19', '', ''),
-(7, 'B00659780', '$2y$10$Veb2k7RSNv2dh6rQrW9Dn.Pvpdcb7yZiDtCQJMP4bbCBpPf.4OWsu', 0, '2018-03-22 16:54:50', '', ''),
-(8, 'B00653456', '$2y$10$yw0FnKD8a7KxNU7LX8x1Vev/cvs38Z7f9uHI6VloFvqV2XJUPbtPe', 0, '2018-03-22 17:04:56', '', '');
+INSERT INTO `users` (`user_id`, `Username`, `password`, `email`, `reg_time`, `first_name`, `surname`) VALUES
+(14, 'B00659788', '$2y$10$M5aYKiqlNJNmhNlU7DbXxOfxELyAZkWQZ9qataw4lScSr62XdKssS', 'ONeill-C98@ulster.ac.uk', '2018-03-28 20:31:24', 'Christopher', 'O\'Neill'),
+(15, 'B00655487', '$2y$10$6Ceo9uZ8bJkvx798TuebVeaSfxHgOGdgbFMtaGOGmq/xk8N8Hxk7G', 'levinaoneill@hotmail.co.uk', '2018-03-28 21:30:05', 'Levina', 'O\'Neill'),
+(18, 'B00659799', '$2y$10$m9WuL6e14eZlksKzIzx0k.MLnDjxMFdag2c0xZjsrSrSflh.4EQ4q', 'ONeill-D@ulster.ac.uk', '2018-03-28 22:29:01', 'Declan', 'ONeill'),
+(19, 'B00659700', '$2y$10$KYfqpHk0iRiE9VeaP4K.0uLJ6zIToMhgI2RwlNPwfKKd4BTjlYGqy', 'Millar-J20@ulster.ac.uk', '2018-03-30 11:46:42', 'John', 'Millar'),
+(20, 'B00659711', '$2y$10$bVlt/MpEaET0fPLc8DfRBOsvkiWEpR3g4koJyQ360W6nZ8RWEBb3W', 'paddysally@ulster.ac.uk', '2018-03-30 11:48:16', 'Paddy ', 'Sally'),
+(21, 'B00659713', '$2y$10$313zJsNewvS7jtryDttcr.f7SBvBu.6iBtLDfQE9zR9utcwRThZwq', 'markdonelly@gmail.com', '2018-04-01 15:48:44', 'Mark', 'Donnelly'),
+(22, 'B00659755', '$2y$10$VTmcOfvuwkQFh8aKOWwjwuvl4SGSy53VM1YCqbH6IIknVipxSd9iy', 'ryancoleman@gmail.com', '2018-04-01 16:00:19', 'Ryan', 'Coleman'),
+(23, 'B00659722', '$2y$10$RQB.Q82KgteGtLfsQ4L7COWV1lDBZlZbkG1g3Rl2P0rNPLFKagEZa', 'ONeillC12@ulster.ac.uk', '2018-04-01 16:03:49', 'Claire', 'ONeill'),
+(24, 'B00659456', '$2y$10$rQ1RvFmdye6.gQhi7MWShedvnvZpy7G255iWnc0FAU44AN.Z0FSpi', 'rorymagee@ulster.ac.uk', '2018-04-01 16:11:09', 'Rory', 'Magee'),
+(25, 'B00659455', '$2y$10$/0Iax7yVNGhU0J2gs6KAkueogSYqIpsNX89xJL1AxrsU8jCtdiAS6', 'conormax@ulster.ac.uk', '2018-04-01 16:16:45', 'Conor', 'Maxwell');
 
 --
 -- Indexes for dumped tables
@@ -103,8 +155,14 @@ INSERT INTO `users` (`user_id`, `Username`, `password`, `email`, `reg_time`, `fi
 -- Indexes for table `assessment`
 --
 ALTER TABLE `assessment`
-  ADD PRIMARY KEY (`ass_id`),
-  ADD UNIQUE KEY `request_id` (`request_id`);
+  ADD PRIMARY KEY (`ass_id`);
+
+--
+-- Indexes for table `pm_user`
+--
+ALTER TABLE `pm_user`
+  ADD PRIMARY KEY (`pm_id`),
+  ADD UNIQUE KEY `pm_username` (`pm_username`);
 
 --
 -- Indexes for table `reason_for_request`
@@ -118,7 +176,8 @@ ALTER TABLE `reason_for_request`
 ALTER TABLE `requests`
   ADD PRIMARY KEY (`request_id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `user_id_2` (`user_id`);
+  ADD KEY `ass_id` (`ass_id`),
+  ADD KEY `rfr_id` (`rfr_id`);
 
 --
 -- Indexes for table `users`
@@ -135,13 +194,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assessment`
 --
 ALTER TABLE `assessment`
-  MODIFY `ass_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ass_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `pm_user`
+--
+ALTER TABLE `pm_user`
+  MODIFY `pm_id` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reason_for_request`
 --
 ALTER TABLE `reason_for_request`
-  MODIFY `rfr_id` int(5) NOT NULL AUTO_INCREMENT COMMENT 'reason for request';
+  MODIFY `rfr_id` int(5) NOT NULL AUTO_INCREMENT COMMENT 'reason for request', AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `requests`
@@ -153,7 +218,19 @@ ALTER TABLE `requests`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id', AUTO_INCREMENT=9;
+  MODIFY `user_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id', AUTO_INCREMENT=26;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `requests`
+--
+ALTER TABLE `requests`
+  ADD CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `requests_ibfk_2` FOREIGN KEY (`ass_id`) REFERENCES `assessment` (`ass_id`),
+  ADD CONSTRAINT `requests_ibfk_3` FOREIGN KEY (`rfr_id`) REFERENCES `reason_for_request` (`rfr_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
